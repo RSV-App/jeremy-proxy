2a7e3f16b459931f7e59623e15136911bf9404f8
Ubuntu <ubuntu@ip-172-31-16-223.us-west-2.compute.internal>
2020-04-17 22:17:15 +0000

    Updates server code and prepares for Redis cache.

--- server/index.js
+++ server/index.js
@@ -4,10 +4,19 @@ const express = require('express');
 const path = require('path');
 const axios = require('axios');
 const app = express();
+const redis = require('redis');
+const redisClient = redis.createClient();
 
 const REVIEWS_SERVER = 'http://54.214.131.42';
+const MENU_SERVER = 'http://54.186.26.38';
+const RESERVATIONS_SERVER = 'http://35.163.170.86';
 
 app.use(express.static(path.resolve(__dirname, '../public')));
+
+app.get('/loaderio-888c9cc15f684f466e7698a5b3a3c193', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'public', 'loaderio-888c9cc15f684f466e7698a5b3a3c193.txt'));
+});
+
 
 app.get('/:id', (req, res) => {
   if (!req.params.id) {
@@ -18,8 +27,41 @@ app.get('/:id', (req, res) => {
   }
 });
 
+
 app.get('/:id/reviews', (req, res) => {
-  axios.get(`${REVIEWS_SERVER}/${req.params.id}/reviews`)
+  redisClient.get(req.params.id, (err, cached) => {
+    if (cached) {
+      res.status(200);
+      res.send(cached);
+    } else {
+      axios.get(`${REVIEWS_SERVER}/${req.params.id}/reviews`)
+        .then((result) => {
+          redisClient.setex(req.params.id, 3600, JSON.stringify(result));
+          res.status(200);
+          res.send(result.data);
+        })
+        .catch((err) => {
+          res.status(500);
+          res.end();
+        }); 
+    }
+  });
+});
+
+app.get('/api/menu/:id', function(req, res) {
+  axios.get(`${MENU_SERVER}/api/menu/${req.params.id}`)
+    .then((result) => {
+      res.status(200);
+      res.send(result.data);
+    })
+    .catch((err) => {
+      res.status(500);
+      res.end();
+    });
+});
+
+app.get('/:id/reservations', function(req, res) {
+  axios.get(`${RESERVATIONS_SERVER}/${req.params.id}/reservations`)
     .then((result) => {
       res.status(200);
       res.send(result.data);
@@ -31,6 +73,8 @@ app.get('/:id/reviews', (req, res) => {
 });
 
 
+
 app.listen(3000, () => {
+
   console.log('Open Table proxy server listening on port 3000!');
 });
